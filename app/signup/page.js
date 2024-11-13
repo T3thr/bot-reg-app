1e2378feafe031ca03a26af1a6aa2639c2e0b11a
T3thr <t.theerapat33@gmail.com>
2024-11-14 06:12:24 +0700

    m

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,8 +1,11 @@
 import Signup from "@/components/Signup";
 import React from "react";
+import { Suspense } from 'react'; 
 
 const SignupPage = () => {
+  <Suspense fallback={<div>Loading...</div>}>
   return <Signup />;
+  </Suspense>
 };
 
 export default SignupPage;
