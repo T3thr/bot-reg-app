55ac5e376828bad6aaf6b83ed7efe44f216861ab
T3thr <t.theerapat33@gmail.com>
2024-11-13 07:27:50 +0700

    m

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -17,7 +17,7 @@ export async function POST(req) {
     const newUser = new User({ username, password, lineUserId });
     await newUser.save();
 
-    return NextResponse.json({ status: 201 });
+    return NextResponse.json({ success: true });
   } catch (error) {
     console.error('Registration error:', error);
     return NextResponse.json({ error: 'Error registering user.' }, { status: 500 });
--- components/Register.jsx
+++ components/Register.jsx
@@ -28,11 +28,11 @@ export default function Register() {
     setMessageType('');
   
     try {
-      const response = await fetch('/api/register', {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password, lineUserId }),
-      });
+    });          
   
       const data = await response.json();
   
