f18213f714cce146e3a4bd813c53f3ceab25b1c9
T3thr <t.theerapat33@gmail.com>
2024-11-14 07:59:27 +0700

    m

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { useRouter  } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import liff from '@line/liff';
 import { FaUserCircle, FaSignOutAlt, FaRegRegistered, FaCheckCircle } from 'react-icons/fa';
 import styles from './Login.module.css';
@@ -10,8 +10,18 @@ export default function Login() {
   const [grades, setGrades] = useState(null);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
+    const code = searchParams.get('code');
+    const state = searchParams.get('state');
+    const liffClientId = searchParams.get('liffClientId');
+    const liffRedirectUri = searchParams.get('liffRedirectUri');
+
+    if (code && state && liffClientId && liffRedirectUri) {
+      localStorage.setItem('liffParams', JSON.stringify({ code, state, liffClientId, liffRedirectUri }));
+    }
+
     liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID }).then(async () => {
       if (liff.isLoggedIn()) {
         const profileData = await liff.getProfile();
