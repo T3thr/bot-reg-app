4b56fbe9e3e14b07f0d9bb4a2f85739ca30c6bc5
T3thr <t.theerapat33@gmail.com>
2024-11-16 05:11:03 +0700

    m

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+      serverActions: true,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/old-url',
+          destination: '/new-url',
+          permanent: true,
+        },
+      ];
+    },
   };
   
   export default nextConfig;
